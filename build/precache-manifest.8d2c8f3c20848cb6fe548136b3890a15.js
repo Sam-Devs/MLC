e1489e99916517fea2aaaaa1a866b5f008f26f3c
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2020-10-24 21:07:20 +0100

    updates

---
+++ build/precache-manifest.8d2c8f3c20848cb6fe548136b3890a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8780a64807b43bb35701904291581a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c186009496abd79d92dc",
+    "url": "/static/css/main.ae544f2e.chunk.css"
+  },
+  {
+    "revision": "ac619ac2da4cd3cf3c38",
+    "url": "/static/js/2.b7f4b76c.chunk.js"
+  },
+  {
+    "revision": "3a5429a82270d0052de0a2aa9965aef7",
+    "url": "/static/js/2.b7f4b76c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c186009496abd79d92dc",
+    "url": "/static/js/main.47c51cde.chunk.js"
+  },
+  {
+    "revision": "5bf19cd4a9b40b88a778",
+    "url": "/static/js/runtime-main.b74b74d7.js"
+  }
+]);
